21c2bb6bf7dbf02fc2426c21be8ff35c206fd20c
ajay1989s <76716669+ajay1989s@users.noreply.github.com>
2023-12-19 15:35:50 +0530

    Update seed_01.groovy

--- seed_01.groovy
+++ seed_01.groovy
@@ -1,5 +1,14 @@
-job('example') {
-  steps {
-    shell('echo Hello World!')
+pipelineJob('job-dsl-plugin') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('git@github.com:ajay1989s/petclinic.git')
+          }
+          branch('*/master')
+        }
+      }
+      lightweight()
+    }
   }
-}
